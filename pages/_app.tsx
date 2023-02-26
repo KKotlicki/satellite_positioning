729d5463f22b8dfa6ec5f2617f7f8d5381e1b8c5
KKotlicki <konrad.kotlicki@gmail.com>
2023-02-26 17:37:20 +0100

    MUI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,23 @@
 import Head from 'next/head'
 import type { AppProps } from 'next/app'
+import { Roboto } from 'next/font/google'
+import { orange } from '@mui/material/colors';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+
+const roboto = Roboto({
+  weight: '400',
+  subsets: ['latin'],
+})
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: orange[500],
+    },
+    mode: 'dark',
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -10,7 +28,13 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <CssBaseline></CssBaseline>
+        <main className={roboto.className}>
+          <Component {...pageProps} />
+        </main>
+      </ThemeProvider>
+
     </>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,16 @@
+import { Button, Card, CardActions, CardContent, CardHeader } from "@mui/material";
+
 export default function Home() {
   return (
     <>
+      <Card sx={{
+        width: 'fit-content',
+        margin: 'auto',
+      }}>
+        <CardHeader title='AAAAAAAAAAA'>AAAAAAAAAAA</CardHeader>
+        <CardContent>BBBBBBBBBBBBB</CardContent>
+        <CardActions><Button>CCCCCCCCCCC</Button></CardActions>
+      </Card>
     </>
   )
 }
