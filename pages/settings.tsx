5e216e67c302d66a48a2f19da9c8f1f0cd99a87a
KKotlicki <konrad.kotlicki@gmail.com>
2023-02-27 09:15:12 +0100

    settings Card UI

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -1,15 +1,52 @@
-import { Button, Card, CardActions, CardContent, CardHeader } from "@mui/material";
+import { Button, Card, CardActions, CardContent, CardHeader, useTheme, TextField } from "@mui/material";
 
 export default function Settings() {
+  const theme = useTheme();
   return (
     <>
       <Card sx={{
         width: 'fit-content',
         margin: 'auto',
-      }}>
-        <CardHeader title='AAAAAAAAAAA'>AAAAAAAAAAA</CardHeader>
-        <CardContent>BBBBBBBBBBBBB</CardContent>
-        <CardActions><Button>CCCCCCCCCCC</Button></CardActions>
+        marginTop: '1rem',
+        
+      }} variant="outlined">
+        <CardHeader title='Settings'
+        style={{ borderBottom: `1px solid ${theme.palette.divider}`, backgroundColor: theme.palette.divider }}></CardHeader>
+        {/* make Material-UI input fields with descriptions on the left: Latitude, Longitude, height, day, time */}
+        <CardContent>
+        <TextField
+          label="Latitude"
+          // InputLabelProps={{ position: "left" }}
+          fullWidth
+          margin="normal"
+        />
+        <TextField
+          label="Longitude"
+          // InputLabelProps={{ position: "left" }}
+          fullWidth
+          margin="normal"
+        />
+        <TextField
+          label="Height"
+          // InputLabelProps={{ position: "left" }}
+          fullWidth
+          margin="normal"
+        />
+        <TextField
+          label="Day"
+          // InputLabelProps={{ position: "left" }}
+          fullWidth
+          margin="normal"
+        />
+        <TextField
+          label="Time"
+          // InputLabelProps={{ position: "left" }}
+          fullWidth
+          margin="normal"
+        />
+      </CardContent>
+
+        <CardActions><Button>Apply</Button></CardActions>
       </Card>
     </>
   )
