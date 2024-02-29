df2b1671aa80df187a9314b5bed0523e2909c438
KKotlicki <konrad.kotlicki@gmail.com>
2024-02-29 13:22:28 +0100

    feat: add SatellitePath type and GPS var implementation

--- store/store.tsx
+++ store/store.tsx
@@ -1,7 +1,13 @@
 import dayjs, { Dayjs } from 'dayjs'
 import { create } from 'zustand'
 
-const useStore = create<{
+type SatellitePath = Record<number, [number, number][]>
+
+// const GPS = {
+//     1: [[0, 0], [0, 0], [0, 0], [0, 0]],
+// } as const satisfies SatellitePath
+
+type Store = {
     latitude: string,
     changeLatitude: (newLatitude: string) => void
     longitude: string,
@@ -14,7 +20,11 @@ const useStore = create<{
     changeTimeAndDate: (newTimeAndDate: Dayjs) => void
     almanacName: string,
     changeAlmanacName: (newAlmanacName: string) => void
-}>((set) => ({
+    GPS: SatellitePath,
+    changeGPS: (newGPS: SatellitePath) => void
+}
+
+const useStore = create<Store>((set) => ({
     latitude: "N 0° 0' 0",
     changeLatitude: (newLatitude) => set(() => ({ latitude: newLatitude })),
     longitude: "E 0° 0' 0",
@@ -27,6 +37,29 @@ const useStore = create<{
     changeTimeAndDate: (newTimeAndDate) => set(() => ({ timeAndDate: newTimeAndDate })),
     almanacName: "",
     changeAlmanacName: (newAlmanacName) => set(() => ({ almanacName: newAlmanacName })),
-}))
+    GPS: {
+        2: Array.from({ length: 144 }, (_, index) => {
+            const angle = (index / 144) * 2 * Math.PI;
+            const latitude = Math.sin(angle) * 100;
+            const longitude = Math.cos(angle) * 100;
+            return [latitude, longitude];
+        }),
+        3: Array.from({ length: 144 }, (_, index) => {
+            const angle = (index / 144) * 2 * Math.PI;
+            const latitude = Math.sin(angle) * 200;
+            const longitude = Math.cos(angle) * 200;
+            return [latitude, longitude];
+        }),
+        4: Array.from({ length: 144 }, (_, index) => {
+            const angle = (index / 144) * 2 * Math.PI;
+            const latitude = Math.sin(angle) * 300;
+            const longitude = Math.cos(angle) * 300;
+            return [latitude, longitude];
+        }),
+    },
+    // GPS: {},
 
-export default useStore 
+    changeGPS: (newGPS) => set(() => ({ GPS: newGPS })),
+}));
+
+export default useStore;
