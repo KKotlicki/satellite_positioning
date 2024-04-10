c2074128629582384ad952f47d4f5848608819db
KKotlicki <konrad.kotlicki@gmail.com>
2024-04-10 05:23:09 +0200

    feat: no almanac settings view card

--- components/settings-view.tsx
+++ components/settings-view.tsx
@@ -1,29 +1,56 @@
 import useStore from "@/store/store";
+import { Paper } from "@mui/material";
 import Box from "@mui/material/Box";
+import { styled } from "@mui/material/styles";
 import { useZustand } from "use-zustand";
 
 
-export default function SettingsView() {
-  const latitude = useZustand(useStore, (state) => state.latitude)
-  const longitude = useZustand(useStore, (state) => state.longitude)
-  const height = useZustand(useStore, (state) => state.height)
-  const elevationCutoff = useZustand(useStore, (state) => state.elevationCutoff)
-  const date = useZustand(useStore, (state) => state.date)
-  const almanacName = useZustand(useStore, (state) => state.almanacName)
-  function parseLatitude(latitude: number) {
-    const direction = latitude < 0 ? "S" : "N"
-    const absolute = Math.abs(latitude)
-    const degrees = Math.floor(absolute)
-    const minutes = (absolute - degrees) * 60
-    return `${degrees}° ${minutes.toFixed(2)}' ${direction}`
+const AlmanacPaper = styled(Paper, { shouldForwardProp: (prop) => prop !== 'color' })(({ theme, color }) => ({
+  flex: '1 0 auto',
+  margin: theme.spacing(1),
+  padding: theme.spacing(2),
+  ...theme.typography.body2,
+  textAlign: 'center',
+  maxWidth: '100%',
+  backgroundColor: color,
+}));
+
+const formatAlmanacName = (name: string): string => {
+  let formattedName: string | undefined = name;
+  if (formattedName?.endsWith(".alm")) {
+    formattedName = formattedName.slice(0, -4);
+  }
+  if (formattedName?.startsWith("Almanac") && formattedName.length > 7) {
+    formattedName = formattedName.slice(7);
+  }
+  return formattedName || "";
+};
+
+export default function SettingsView(): JSX.Element {
+
+  const { latitude, longitude, height, elevationCutoff, date, almanacName } = useZustand(useStore, (state) => ({
+    latitude: state.latitude,
+    longitude: state.longitude,
+    height: state.height,
+    elevationCutoff: state.elevationCutoff,
+    date: state.date,
+    almanacName: state.almanacName
+  }));
+
+  function parseLatitude(latitude: number): string {
+    const direction = latitude < 0 ? "S" : "N";
+    const absolute = Math.abs(latitude);
+    const degrees = Math.floor(absolute);
+    const minutes = (absolute - degrees) * 60;
+    return `${degrees}° ${minutes.toFixed(2)}' ${direction}`;
   }
 
-  function parseLongitude(longitude: number) {
-    const direction = longitude < 0 ? "W" : "E"
-    const absolute = Math.abs(longitude)
-    const degrees = Math.floor(absolute)
-    const minutes = (absolute - degrees) * 60
-    return `${degrees}° ${minutes.toFixed(2)}' ${direction}`
+  function parseLongitude(longitude: number): string {
+    const direction = longitude < 0 ? "W" : "E";
+    const absolute = Math.abs(longitude);
+    const degrees = Math.floor(absolute);
+    const minutes = (absolute - degrees) * 60;
+    return `${degrees}° ${minutes.toFixed(2)}' ${direction}`;
   }
 
   return (
@@ -41,7 +68,9 @@ export default function SettingsView() {
       <li>Height: {height} m</li>
       <li>Elevation cutoff: {elevationCutoff}°</li>
       <li>Date: {date.format("DD/MM/YYYY")}</li>
-      <li>Almanac:{`\n${almanacName}`}</li>
+      <AlmanacPaper color={almanacName ? 'green' : 'red'}>
+        {formatAlmanacName(almanacName) || "No Almanac"}
+      </AlmanacPaper>
     </Box>
-  )
+  );
 }
