f5f05d81e15a74493366f43cf8124e3cbe4340e9
KKotlicki <konrad.kotlicki@gmail.com>
2024-06-11 16:45:28 +0200

    feat: extended lat/lon formatting

--- components/settings/latitude.tsx
+++ components/settings/latitude.tsx
@@ -2,89 +2,79 @@ import { useNavigationActions } from "@/stores/navigation-store";
 import { InputAdornment, TextField } from "@mui/material";
 import { type ChangeEvent, useState } from "react";
 
-
 export default function LatitudePicker() {
   const [latitudeValue, setLatitudeValue] = useState("N 0° 0' 0.0")
 
   const { changeLatitude } = useNavigationActions()
 
   const isValidLatitude = (value: string) => {
-    const sanitizedValue = value.replace(/\s+/g, "")
+    const sanitizedValue = value.replace(/\s+/g, "").replace("''", "'")
 
     if (
-      /^[-NnSs]?\d{1,2}°\d{1,2}'\d{1,2}(\.\d+)?$|^[-NnSs]?\d+(\.\d+)?$/.test(
+      /^[-NnSs]?\d{1,2}°\d{1,2}'\d{1,2}(\.\d+)?['']?[NnSs]?$|^[-NnSs]?\d+(\.\d+)?['']?[NnSs]?$/.test(
         sanitizedValue
       )
     ) {
       if (sanitizedValue.includes("°")) {
         const parts = sanitizedValue.match(
-          /([-NnSs]?)(\d{1,2})°(\d{1,2})'(\d{1,2}(\.\d+)?)/
+          /([-NnSs]?)(\d{1,2})°(\d{1,2})'(\d{1,2}(\.\d+)?['']?)([NnSs]?)/
         )
 
         if (parts === null) return false
 
-        const third = parts[2]
-        const fourth = parts[3]
-        const fifth = parts[4]
+        const degrees = parts[2]
+        const minutes = parts[3]
+        const seconds = parts[4]
 
-        if (third === undefined || fourth === undefined || fifth === undefined) return false
+        if (degrees === undefined || minutes === undefined || seconds === undefined) return false
 
-        if (parts) {
-          const degrees = Number.parseFloat(third)
-          const minutes = Number.parseFloat(fourth) / 60
-          const seconds = Number.parseFloat(fifth) / 3600
-          const totalDegrees = degrees + minutes + seconds
-          return totalDegrees <= 90
-        }
-      } else {
-        const parts = sanitizedValue.match(/([-NnSs]?)(\d+(\.\d+)?)/)
+        const totalDegrees = Number.parseFloat(degrees) + Number.parseFloat(minutes) / 60 + Number.parseFloat(seconds) / 3600
+        return totalDegrees <= 90
+      }
+        const parts = sanitizedValue.match(/([-NnSs]?)(\d+(\.\d+)?)([NnSs]?)?/)
 
         if (parts === null) return false
 
-        const third = parts[2]
+        const degrees = parts[2]
 
-        if (third === undefined) return false
+        if (degrees === undefined) return false
 
-        if (parts) {
-          const value = Number.parseFloat(third)
-          return value <= 90
-        }
-      }
+        const totalDegrees = Number.parseFloat(degrees)
+        return totalDegrees <= 90
     }
     return false
   }
 
   const parseLatitude = (value: string) => {
-    const sanitizedValue = value.replace(/\s+/g, "")
+    const sanitizedValue = value.replace(/\s+/g, "").replace("''", "'")
     if (
-      /^[-NnSs]?\s?\d{1,2}°\s?\d{1,2}'\s?\d{1,2}(\.\d+)?$/.test(sanitizedValue)
+      /^[-NnSs]?\s?\d{1,2}°\s?\d{1,2}'\s?\d{1,2}(\.\d+)?['']?[NnSs]?$/.test(sanitizedValue)
     ) {
       if (sanitizedValue.includes("°")) {
         const parts = sanitizedValue.match(
-          /([-NnSs]?)\s?(\d{1,2})°\s?(\d{1,2})'\s?(\d{1,2}(\.\d+)?)?/
+          /([-NnSs]?)\s?(\d{1,2})°\s?(\d{1,2})'\s?(\d{1,2}(\.\d+)?['']?)?([NnSs]?)/
         )
         if (parts) {
-          const second = parts[1]
-          const third = parts[2]
-          if (second === undefined || third === undefined) return Number.NaN
-          const degrees = Number.parseFloat(third)
+          const signPart = parts[1] || parts[6]
+          const degrees = parts[2]
+          if (signPart === undefined || degrees === undefined) return Number.NaN
           const minutes = parts[3] ? Number.parseFloat(parts[3]) / 60 : 0
           const seconds = parts[4] ? Number.parseFloat(parts[4]) / 3600 : 0
-          const sign = /^-|S|s/.test(second) ? -1 : 1
-          return sign * (degrees + minutes + seconds)
+          const sign = /^-|S|s/.test(signPart) ? -1 : 1
+          return sign * (Number.parseFloat(degrees) + minutes + seconds)
         }
       }
     } else if (
       /^[-NnSs]?\s?\d+(\.\d+)?$/.test(sanitizedValue) ||
       /^[-NnSs]?\s?\d{1,2}$/.test(sanitizedValue)
     ) {
-      const parts = sanitizedValue.match(/([-NnSs]?)\s?(\d+(\.\d+)?)/)
+      const parts = sanitizedValue.match(/([-NnSs]?)\s?(\d+(\.\d+)?)([NnSs]?)?/)
       if (parts) {
-        const second = parts[1]
-        const third = parts[2]
-        if (second === undefined || third === undefined) return Number.NaN
-        const sign = /^-|S|s/.test(second) ? -1 : 1
-        return sign * Number.parseFloat(third)
+        const signPart = parts[1] || parts[4]
+        const degrees = parts[2]
+        if (signPart === undefined || degrees === undefined) return Number.NaN
+        const sign = /^-|S|s/.test(signPart) ? -1 : 1
+        return sign * Number.parseFloat(degrees)
       }
     }
     return Number.NaN
--- components/settings/longitude.tsx
+++ components/settings/longitude.tsx
@@ -4,112 +4,104 @@ import { type ChangeEvent, useState } from "react";
 
 
 export default function LongitudePicker() {
-	const [longitudeValue, setLongitudeValue] = useState("E 0° 0' 0.0")
+  const [longitudeValue, setLongitudeValue] = useState("E 0° 0' 0.0")
 
-	const { changeLongitude } = useNavigationActions()
+  const { changeLongitude } = useNavigationActions()
 
-	const isValidLongitude = (value: string) => {
-		const sanitizedValue = value.replace(/\s+/g, "")
+  const isValidLongitude = (value: string) => {
+    const sanitizedValue = value.replace(/\s+/g, "").replace("''", "'")
 
-		if (
-			/^[-EeWw]?\d{1,3}°\d{1,2}'\d{1,2}(\.\d+)?$|^[-EeWw]?\d{1,3}(\.\d+)?$/.test(
-				sanitizedValue
-			)
-		) {
-			if (sanitizedValue.includes("°")) {
-				const parts = sanitizedValue.match(
-					/([-EeWw]?)(\d{1,3})°(\d{1,2})'(\d{1,2}(\.\d+)?)/
-				)
-				if (parts) {
-					const third = parts[2]
-					const fourth = parts[3]
-					const fifth = parts[4]
+    if (
+      /^[-EeWw]?\d{1,3}°\d{1,2}'\d{1,2}(\.\d+)?['']?[EeWw]?$|^[-EeWw]?\d{1,3}(\.\d+)?['']?[EeWw]?$/.test(
+        sanitizedValue
+      )
+    ) {
+      if (sanitizedValue.includes("°")) {
+        const parts = sanitizedValue.match(
+          /([-EeWw]?)(\d{1,3})°(\d{1,2})'(\d{1,2}(\.\d+)?['']?)([EeWw]?)/
+        )
 
-					if (third === undefined || fourth === undefined || fifth === undefined) return false
+        if (parts === null) return false
 
-					const degrees = Number.parseFloat(third)
-					const minutes = Number.parseFloat(fourth) / 60
-					const seconds = Number.parseFloat(fifth) / 3600
-					const totalDegrees = degrees + minutes + seconds
-					return totalDegrees <= 180 && totalDegrees >= -180
-				}
-			} else {
-				const parts = sanitizedValue.match(/([-EeWw]?)(\d{1,3}(\.\d+)?)/)
-				if (parts) {
-					const second = parts[1]
-					const third = parts[2]
+        const degrees = parts[2]
+        const minutes = parts[3]
+        const seconds = parts[4]
 
-					if (second === undefined || third === undefined) return false
+        if (degrees === undefined || minutes === undefined || seconds === undefined) return false
 
-					const value = Number.parseFloat(third)
-					const sign = /^-|W|w/.test(second) ? -1 : 1
-					const totalDegrees = sign * value
-					return totalDegrees <= 180 && totalDegrees >= -180
-				}
-			}
-		}
-		return false
-	}
+        const totalDegrees = Number.parseFloat(degrees) + Number.parseFloat(minutes) / 60 + Number.parseFloat(seconds) / 3600
+        return totalDegrees <= 180
+      }
+        const parts = sanitizedValue.match(/([-EeWw]?)(\d{1,3}(\.\d+)?)([EeWw]?)?/)
 
-	const parseLongitude = (value: string) => {
-		const sanitizedValue = value.replace(/\s+/g, "")
+        if (parts === null) return false
 
-		if (
-			/^[-EeWw]?\d{1,3}°\d{1,2}'\d{1,2}(\.\d+)?$|^[-EeWw]?\d{1,3}(\.\d+)?$/.test(
-				sanitizedValue
-			)
-		) {
-			if (sanitizedValue.includes("°")) {
-				const parts = sanitizedValue.match(
-					/([-EeWw]?)(\d{1,3})°(\d{1,2})'(\d{1,2}(\.\d+)?)?/
-				)
-				if (parts) {
-					const second = parts[1]
-					const third = parts[2]
-					if (second === undefined || third === undefined) return Number.NaN
-					const degrees = Number.parseFloat(third)
-					const minutes = parts[3] ? Number.parseFloat(parts[3]) / 60 : 0
-					const seconds = parts[4] ? Number.parseFloat(parts[4]) / 3600 : 0
-					const sign = /^-|W|w/.test(second) ? -1 : 1
-					const totalDegrees = sign * (degrees + minutes + seconds)
-					return Math.max(Math.min(totalDegrees, 180), -180)
-				}
-			} else {
-				const parts = sanitizedValue.match(/([-EeWw]?)(\d{1,3}(\.\d+)?)/)
-				if (parts) {
-					const second = parts[1]
-					const third = parts[2]
-					if (second === undefined || third === undefined) return Number.NaN
-					const sign = /^-|W|w/.test(second) ? -1 : 1
-					const value = Number.parseFloat(third)
-					return Math.max(Math.min(sign * value, 180), -180)
-				}
-			}
-		}
-		return Number.NaN
-	}
+        const degrees = parts[2]
 
-	const handleLongitudeChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-		const newValue = e.target.value
-		setLongitudeValue(newValue)
+        if (degrees === undefined) return false
 
-		if (isValidLongitude(newValue)) {
-			changeLongitude(parseLongitude(newValue))
-		}
-	}
+        const totalDegrees = Number.parseFloat(degrees)
+        return totalDegrees <= 180
+    }
+    return false
+  }
 
-	return (
-		<TextField
-			label='Longitude'
-			value={longitudeValue}
-			onChange={handleLongitudeChange}
-			fullWidth
-			margin='normal'
-			placeholder="E 0° 0' 0.0"
-			InputProps={{
-				endAdornment: <InputAdornment position='end'>°</InputAdornment>
-			}}
-			error={!isValidLongitude(longitudeValue)}
-		/>
-	)
+  const parseLongitude = (value: string) => {
+    const sanitizedValue = value.replace(/\s+/g, "").replace("''", "'")
+    if (
+      /^[-EeWw]?\s?\d{1,3}°\s?\d{1,2}'\s?\d{1,2}(\.\d+)?['']?[EeWw]?$/.test(sanitizedValue)
+    ) {
+      if (sanitizedValue.includes("°")) {
+        const parts = sanitizedValue.match(
+          /([-EeWw]?)\s?(\d{1,3})°\s?(\d{1,2})'\s?(\d{1,2}(\.\d+)?['']?)?([EeWw]?)/
+        )
+        if (parts) {
+          const signPart = parts[1] || parts[6]
+          const degrees = parts[2]
+          if (signPart === undefined || degrees === undefined) return Number.NaN
+          const minutes = parts[3] ? Number.parseFloat(parts[3]) / 60 : 0
+          const seconds = parts[4] ? Number.parseFloat(parts[4]) / 3600 : 0
+          const sign = /^-|W|w/.test(signPart) ? -1 : 1
+          return sign * (Number.parseFloat(degrees) + minutes + seconds)
+        }
+      }
+    } else if (
+      /^[-EeWw]?\s?\d+(\.\d+)?$/.test(sanitizedValue) ||
+      /^[-EeWw]?\s?\d{1,3}$/.test(sanitizedValue)
+    ) {
+      const parts = sanitizedValue.match(/([-EeWw]?)\s?(\d+(\.\d+)?)([EeWw]?)?/)
+      if (parts) {
+        const signPart = parts[1] || parts[4]
+        const degrees = parts[2]
+        if (signPart === undefined || degrees === undefined) return Number.NaN
+        const sign = /^-|W|w/.test(signPart) ? -1 : 1
+        return sign * Number.parseFloat(degrees)
+      }
+    }
+    return Number.NaN
+  }
+
+  const handleLongitudeChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const newValue = e.target.value
+    setLongitudeValue(newValue)
+
+    if (isValidLongitude(newValue)) {
+      changeLongitude(parseLongitude(newValue))
+    }
+  }
+
+  return (
+    <TextField
+      label='Longitude'
+      value={longitudeValue}
+      onChange={handleLongitudeChange}
+      fullWidth
+      margin='normal'
+      placeholder="E 0° 0' 0.0"
+      InputProps={{
+        endAdornment: <InputAdornment position='end'>°</InputAdornment>
+      }}
+      error={!isValidLongitude(longitudeValue)}
+    />
+  )
 }
